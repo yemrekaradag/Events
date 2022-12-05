cf9fbe390ce91336028a23c643bcd35cf8667f4d
Yunus Emre Karadağ <yemrekaradag@hotmail.com>
2022-12-05 15:55:16 +0300

    init

--- Events/Program.cs
+++ Events/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public delegate void Notify();  // delegate
 
+public class ProcessBusinessLogic
+{
+    public event Notify ProcessCompleted; // event
+
+    public void StartProcess()
+    {
+        Console.WriteLine("Process Started!");
+        // some code here..
+        OnProcessCompleted();
+    }
+
+    protected virtual void OnProcessCompleted() //protected virtual method
+    {
+        //if ProcessCompleted is not null then call delegate (raise the event)
+        ProcessCompleted?.Invoke();
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        ProcessBusinessLogic bl = new ProcessBusinessLogic();
+        bl.ProcessCompleted += bl_ProcessCompleted; // register with an event
+        bl.StartProcess();
+    }
+
+    // event handler
+    public static void bl_ProcessCompleted()
+    {
+        Console.WriteLine("Process Completed (from Event Handler)!");
+    }
+}

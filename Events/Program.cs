bd8d6893990508efc9759925e19bf19221120a9a
Yunus Emre Karadağ <yemrekaradag@hotmail.com>
2022-12-05 16:41:05 +0300

    added number class

--- Events/Program.cs
+++ Events/Program.cs
@@ -1,20 +1,22 @@
-﻿public delegate void Notify();  // delegate
+﻿
 
-public class ProcessBusinessLogic
+public delegate void dg_OddNumber(); //delegate
+
+public class Number
 {
-    public event Notify ProcessCompleted; // event
+    public event dg_OddNumber ev_OddNumber; // event
 
-    public void StartProcess()
+    public void Add(int number)
     {
-        Console.WriteLine("Process Started!");
-        // some code here..
-        OnProcessCompleted();
+        Console.WriteLine($"Process Started!\n{number.ToString()}");
+        OnProcessCompleted(number);
     }
 
-    protected virtual void OnProcessCompleted() //protected virtual method
+    protected virtual void OnProcessCompleted(int number) //protected virtual method
     {
-        //if ProcessCompleted is not null then call delegate (raise the event)
-        ProcessCompleted?.Invoke();
+        //Check if number is odd number and ev_OddNumber is not null then call delegate (raise the event)
+        if ((number % 2 != 0))
+            ev_OddNumber?.Invoke(); //Raised Event
     }
 }
 
@@ -22,14 +24,29 @@ class Program
 {
     public static void Main()
     {
-        ProcessBusinessLogic bl = new ProcessBusinessLogic();
-        bl.ProcessCompleted += bl_ProcessCompleted; // register with an event
-        bl.StartProcess();
+        Number num = new Number();
+        int number = 0;
+        //num.ev_OddNumber += new dg_OddNumber(EventMessage);
+
+        //Event gets binded with delegates
+        num.ev_OddNumber += EventMessage; // register with an event
+
+        while (true)
+        {
+            if (int.TryParse(Console.ReadLine(), out number))
+                num.Add(number);
+            else
+                break;
+        }
+
+
+        Console.Read();
     }
 
-    // event handler
-    public static void bl_ProcessCompleted()
+    //Delegates calls this method when event raised.
+    //event handler
+    public static void EventMessage()
     {
-        Console.WriteLine("Process Completed (from Event Handler)!");
+        Console.WriteLine("********Event Executed(from Event Handler) : This is Odd Number**********");
     }
 }
